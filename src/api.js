5dbdbdced5684de2022744107c06b916c553d385
haoq02 <haonguyen492@gmail.com>
2025-03-09 15:08:54 +0700

    fix fd

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:9000';
-
-// Tạo instance của axios với URL cơ bản
-export const api = axios.create({
-    baseURL: API_URL,
-    withCredentials: true, // Để axios gửi cookie của session
-});
